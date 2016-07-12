bbc0146413c62cbee1259b1bf41a429ab39a52d3
David Guben <david.guben@rutgers.edu>
2016-07-12 13:41:14 -0400

    trying to add tweets

--- liri.js
+++ liri.js
@@ -1,1 +1,10 @@
-var twitter = ('keys.js');
+var keys = ('keys.js');
+var Twitter = require('twitter');
+
+
+var params = {screen_name: 'nodejs'};
+exports.twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response){
+  if (!error) {
+    console.log(tweets);
+  }
+});
